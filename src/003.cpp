0be00621d583bc1e7029993d51ad7afca34bbb05
文优 <yi.li@hipac.cn>
2021-02-26 00:18:32 +0800

    add 4

--- src/003.cpp
+++ src/003.cpp
@@ -5,12 +5,26 @@
 #include <set>
 #include <string>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        return 0;
+        vector<char> vec;
+        int max = 0;
+        for (string::iterator iterator = s.begin(); iterator != s.end(); ++iterator) {
+            char c = *iterator;
+            vector<char>::iterator index = find(vec.begin(), vec.end(), c);
+            if (index != vec.end()) {
+                vec.erase(vec.begin(), index + 1);
+            }
+            vec.push_back(*iterator);
+            if (vec.size() > max) {
+                ++max;
+            }
+        }
+        return max;
     }
 };
