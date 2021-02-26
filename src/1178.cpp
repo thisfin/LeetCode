cb50b7eea18a5f2ec980cff0e4c764fb532dfd9b
文优 <yi.li@hipac.cn>
2021-02-26 16:23:32 +0800

    add 1178

--- src/1178.cpp
+++ src/1178.cpp
@@ -4,24 +4,43 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    vector<int> findNumOfValidWords(vector<string>& words, vector<string>& puzzles) {
-        vector<int> a;
-        return a
+    vector<int> findNumOfValidWords(vector<string> &words, vector<string> &puzzles) {
+        unordered_map<int, int> hash; // 使用 hash 存储可以去重
+        for (string word : words) {
+            hash[sToInt(word)]++;
+        }
+
+        vector<int> result;
+        for (string puzzle: puzzles) {
+            int i = 0;
+            int first = cToInt(puzzle[0]);
+            int puzzle2 = sToInt(puzzle);
+            for (int num = puzzle2; num; num = (num - 1) & puzzle2) { // 这种遍历的方式最快
+                if ((first & num) == first) {
+                    i += hash[num];
+                }
+            }
+            result.push_back(i);
+        }
+        return result;
     }
 
-    int toInt(word string) {
-        for (char c: string) {
-
+private:
+    int sToInt(string word) {
+        int i = 0;
+        for (char c: word) {
+            i |= cToInt(c);
         }
-        return 0;
+        return i;
     }
 
     int cToInt(char c) {
-        return (int)c - 97;
+        return 1 << ((int) c - 97);
     }
 };
