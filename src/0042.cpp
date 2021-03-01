10b3f988c50dc012b57e0b44090b26adf313fdcf
文优 <yi.li@hipac.cn>
2021-03-01 19:30:43 +0800

    update 42

--- src/0042.cpp
+++ src/0042.cpp
@@ -9,6 +9,34 @@ using namespace std;
 class Solution {
 public:
     int trap(vector<int> &height) {
+        int lMax = 0;
+        int rMax = 0;
+        int result = 0;
+
+        // 动态找最高点, 两端指针遍历
+        for (int left = 0, right = height.size() - 1; left < right;) {
+            int hr = height[right];
+            int hl = height[left];
+            if (hr > hl) {
+                if (hl <= lMax) {
+                    result += (lMax - hl);
+                } else {
+                    lMax = hl;
+                }
+                left++;
+            } else {
+                if (hr <= rMax) {
+                    result += (rMax - hr);
+                } else {
+                    rMax = hr;
+                }
+                right--;
+            }
+        }
+        return result;
+    }
+
+    int trapOld(vector<int> &height) {
         int lastMaxIndex = 0;
         int maxValue = 0;
         int result = 0;
@@ -45,6 +73,7 @@ public:
             }
         }
 
+        // 可以优化为左右两边一同遍历, 找最高点, 单次循环, 双指针
         return result;
     }
 };
