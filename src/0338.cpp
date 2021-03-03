679eed38b0e5083d55133e5d04328f24fd2e12a8
文优 <yi.li@hipac.cn>
2021-03-03 22:32:09 +0800

    add 338

---
+++ src/0338.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 李毅 on 2021/3/3.
+//
+
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> countBits(int num) {
+        return {0};
+    }
+};
