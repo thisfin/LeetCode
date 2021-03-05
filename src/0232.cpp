8d8c5809ec0bacfb6ad8dde05096c90a74cd69c1
文优 <yi.li@hipac.cn>
2021-03-05 15:45:11 +0800

    update 232

--- src/0232.cpp
+++ src/0232.cpp
@@ -2,31 +2,47 @@
 // Created by 李毅 on 2021/3/5.
 //
 
+#include <stack>
+
+using namespace std;
+
 class MyQueue {
 public:
+    stack<int> header;
+    stack<int> footer;
+
     /** Initialize your data structure here. */
     MyQueue() {
-
     }
 
     /** Push element x to the back of queue. */
     void push(int x) {
-
+        while (!footer.empty()) {
+            header.push(footer.top());
+            footer.pop();
+        }
+        header.push(x);
     }
 
     /** Removes the element from in front of queue and returns that element. */
     int pop() {
+        int result = peek();
+        footer.pop();
+        return result;
 
     }
 
     /** Get the front element. */
     int peek() {
-
+        while (!header.empty()) {
+            footer.push(header.top());
+            header.pop();
+        }
+        return footer.top();
     }
 
-    /** Returns whether the queue is empty. */
     bool empty() {
-
+        return header.empty() && footer.empty();
     }
 };
 
