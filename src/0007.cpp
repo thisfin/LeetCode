48dae1d63431ea108290cc155b4e29b4d085fec7
文优 <yi.li@hipac.cn>
2021-03-23 23:18:25 +0800

    add 0007

---
+++ src/0007.cpp
@@ -0,0 +1,24 @@
+//
+// Created by 李毅 on 2021/3/23.
+//
+
+#include <limits>
+
+using namespace std;
+
+class Solution {
+public:
+    int reverse(int x) {
+        int source = x;
+        __int64_t result = 0;
+        for (; source != 0; ) {
+            int temp = source % 10;
+            source = source / 10;
+            result = result * 10 + temp;
+            if (result > numeric_limits<__int32_t>::max() || result < numeric_limits<__int32_t>::min()) {
+                return 0;
+            }
+        }
+        return result;
+    }
+};
